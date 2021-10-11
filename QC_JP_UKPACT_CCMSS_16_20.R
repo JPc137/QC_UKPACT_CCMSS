3434158c158a44959b8fd57a20a0ea5a69fd1c20
Juan Pablo Avila <56082208+JPc137@users.noreply.github.com>
2021-10-11 18:18:39 -0500

    Segundo _commit poly rev2
    Se creó Poly rev2 se arregló el orden de ejecución del Script  y merge

--- QC_JP_UKPACT_CCMSS_16_20.R
+++ QC_JP_UKPACT_CCMSS_16_20.R
@@ -3,18 +3,18 @@ install.packages(pacman)
 #instalar/Cargar paquetes
 pacman::p_load(sf, tidyverse, dplyr)
 
-# crear variable para guardar nombre  y leer Shape
-poligonos <- "Chiapas_Pol682_Cuadr291_AMG.shp"
-name <- str_remove(poligonos,".shp")
-#Leer tabla de correspondencia con IPCC
+#Leer tabla de correspondencia con IPCC para despues hacer el join
 setwd("C:/UTEMRV_JP/LCC_2016_2020_UKPACT_CCMSS/UKPACT_CHIAP_CAMP_TAB")
-tabl_corresp <- read.csv(as.character("tabl_SAMOF_IPCC_INEGI_FINAL6.csv"))
+tab_corresp <- read.csv(as.character("tabl_SAMOF_IPCC_INEGI_FINAL_99.csv"))
 
-#cambiar work directory donde está el shp a trabajar
+# crear variable para guardar nombre  y leer Shape
+poligonos <- "VMVS_CUAD_979.shp"
+name <- str_remove(poligonos,".shp")
+
+#  *****CAMBIAR EL WORK DIRECTORY****    ruta del shp a trabajar
 poly <- st_read(poligonos)
 
 #HACER FILTRO PARA RECONOCER "LC2016" =0 Y PONER ALGUNA CATEGORIA
-
 #1 = InCorrecto O POSIBLE CORRECCION DE LC 2016 , todo debe ser 0
 poly_rev <- mutate(poly,c_igual_mal = case_when(Val_Cambio  == "C" & Val_T1  ==  Val_T2 ~ 1 , TRUE ~ 0),
                         an_mal = case_when(Val_Cambio  ==  'C' &  is.na(C_Anio) ~ 1 , TRUE ~ 0),
@@ -22,19 +22,31 @@ poly_rev <- mutate(poly,c_igual_mal = case_when(Val_Cambio  == "C" & Val_T1  == 
                         pr_16 = case_when(Val_Cambio == 'P' & Val_T1 != Val_T2 & is.na(C_Anio) ~ 1 , TRUE ~ 0),
                         pr_16_0 = case_when(Val_Cambio == 'P' & Val_T1 != Val_T2 &  C_Anio == 0 ~ 1 , TRUE ~ 0),
                         pr_16_2 = case_when(Val_Cambio == 'P' & Val_T1 == Val_T2 & is.na(C_Anio) ~ 1 , TRUE ~ 0),
-                        pr_16_2_0 = case_when(Val_Cambio == 'P' & Val_T1 != Val_T2 &  C_Anio == 0 ~ 1 , TRUE ~ 0),
-                        #cr_16 = case_when(Val_Cambio == 'C' & Val_T1 != lc_ipcc & is.na(C_Anio) ~ 1 , TRUE ~ 0),
-                        #cr_16_0 = case_when(Val_Cambio == 'C' & Val_T1 != lc_ipcc & C_Anio == 0 ~ 1 , TRUE ~ 0),
+                        pr_16_2_0 = case_when(Val_Cambio == 'P' & Val_T1 == Val_T2 &  C_Anio == 0 ~ 1 , TRUE ~ 0),
                         p_mal = case_when(Val_Cambio == 'P' & Val_T1 == Val_T2 & C_Anio > 0 ~ 1 , TRUE ~ 0),
                         f_c_an = case_when(Val_Cambio == 'C' & is.na(C_Anio) ~ 1 , TRUE ~ 0),
                         val_c = case_when(Val_Cambio %in% c("P", "C")  ~ 0 , TRUE ~ 1), 
                         r_t1 = case_when(Val_T1 %in% c(1:6, 12, 22)  ~ 0 , TRUE ~ 1), #V 0.2 completar , si son  0 me tiene que decir
                         r_t2 = case_when(Val_T2 %in% c(1:6, 12, 22)  ~ 0 , TRUE ~ 1), #V 0.2 completar , si son  0 me tiene que decir
                         r_an = case_when(C_Anio %in% c(17:20,2017, 2018, 2019,2020) ~ 0 , TRUE ~ 1))
-#para identificar cambios != lc_16 
-        #cr_16 = case_when(Val_Cambio == 'C' & Val_T1 != lc_ipcc & is.na(C_Anio)  ~ 1 , TRUE ~ 0),
-#lc_ipcc = case_when(LC_2016  < 21  ~ 1 , TRUE ~ 0),
 
+#crear join (merge)  con la tabla de correspondencia
+tab_merge <- merge(poly_rev, tab_corresp, by.x = "LC_2016", by.y = "clase_samof", all=FALSE) # False = solo se pegan valores existentes TRUE = pega todo
+head(tab_merge)
+
+#para identificar cambios != lc_16 
+poly_rev2 <- mutate(tabl_merge, dif_16_c = case_when(Val_Cambio == 'C' & Val_T1 != val_ipcc & is.na(C_Anio)  ~ 1 , TRUE ~ 0))
+names(poly_rev2$dif_16_c)
+head(tabl_merge)
+###lc_ipcc = case_when(LC_2016  < 21  ~ 1 , TRUE ~ 0),
+
+#cr_16 = case_when(Val_Cambio == 'C' & Val_T1 != lc_ipcc & is.na(C_Anio) ~ 1 , TRUE ~ 0),
+#cr_16_0 = case_when(Val_Cambio == 'C' & Val_T1 != lc_ipcc & C_Anio == 0 ~ 1 , TRUE ~ 0),
+
+
+
+
+#===================================================================================
 #Aplicar Revisión automática 
 table(poly_rev$c_igual_mal)
 table(poly_rev$an_mal)
@@ -59,13 +71,12 @@ table(poly_rev$r_t2)
 table(poly_rev$r_an)
 table(poly_rev$C_Anio)
 
-names(tabl_merge)
-head(poly_rev)
+names(poly_rev)
 
-#crear join (merge)  con la tabla de correspondencia
-tabl_merge <- merge(poly_rev, tabl_corresp, by.x = "LC2016", by.y = "clase_samof", all=FALSE) # False = solo se pegan valores existentes TRUE = pega todo
-dim(tabl_merge)
 
+
+
+#Condicionantes despues del join con "clase_samof"  para obtener el "val_ipcc
 #crear reporte en excel
 write_csv (tabl_merge, paste0(name,"_all.csv"))
 #CREAR SHAPE
